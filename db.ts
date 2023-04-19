1a56b96508f72d1efecb290ee237080cee3ef341
robertnmoffat <robertnmoffat@gmail.com>
2023-04-19 16:32:19 -0700

    Created database functions and routing for customer get requests.

--- db.ts
+++ db.ts
@@ -1,6 +1,12 @@
+/**
+ * Class which handles all MySQL database interaction
+ */
 export default class DB {
-	con: any;
+	con: any;//The database connection.
 
+	/**
+	 * Sets up the connection to the MySQL database
+	 */
 	constructor() {
 		console.log("Calling constructor.");
 		let mysql = require('mysql');
@@ -16,11 +22,84 @@ export default class DB {
 			if (err) throw err;
 			console.log('connected!');
 		});
+
+		this.con.on('error', function (error) {
+			console.log("Database Error:" + error);
+		});
 	}
 
-	public getCustomers() {
+	/**
+	 * Get all customers from database
+	 * @returns JSON array of all customers contained in the database
+	 */
+	public getAllCustomers() {
 		let dbdata: any;
 		let query: string = "SELECT * FROM customers";
+
+		dbdata = new Promise((resolve, reject) => {
+			try {
+				this.con.query(query, function (err, result, fields) {
+					if (err) {
+						throw err;
+						reject();
+					}
+
+					resolve(result);
+				});
+			} catch (err) {
+				console.log(err);
+				resolve([]);
+			}
+		});
+
+
+		return dbdata;
+	}
+
+	/**
+	 * Selects a defined size array of customers from a defined position
+	 * @param start Position to start selecting customers from
+	 * @param finish How many customers to select from the start position
+	 * @returns JSON array of 'count' customers beginning at 'start'
+	 */
+	public getCustomers(start: number, count: number) {
+		let dbdata: any;
+		let query: string = "SELECT * FROM customers LIMIT " + start + ", " + count;
+
+		console.log(query);
+
+		try {
+			dbdata = new Promise((resolve, reject) => {
+
+				if (start < 0 || count <= 0)//Shouldn't get to this point, but just in case
+					reject(new Error("Pagination parameters out of bounds."));
+
+				this.con.query(query, function (err, result, fields) {
+					if (err) {
+						reject(new Error("SQL error."));
+						//throw err;
+					}
+
+					resolve(result);
+				});
+
+			});
+		} catch (err) {
+			console.log(err);
+		}
+
+
+		return dbdata;
+	}
+
+	/**
+	 * Returns a JSON of customer matching the id passed
+	 * @param id customerNumber of the desired customer to be returned
+	 * @returns customer matching the id passed
+	 */
+	public getCustomer(id: number) {
+		let dbdata: any;
+		let query: string = "SELECT * FROM customers WHERE customerNumber = " + id;
 
 		dbdata = new Promise((resolve, reject) => {
 			this.con.query(query, function (err, result, fields) {
--- index.ts
+++ index.ts
@@ -1,20 +1,67 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 import DB from "./db.js";
 const app = express();
 const port: number = 4444;
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 let db: DB = new DB();
 
-app.get("/", (req, res)=>{
-	db.getCustomers().then((r)=>{
-		res.json(r);
+/**
+ * Home route
+ */
+app.get("/", (req, res) => {
+	res.send("Home");
+});
+
+/**
+ * Responds with an array of all customers in database
+ */
+app.get("/api/v1/customers", (req, res) => {
+	//Get all with pagination filters
+	if (req.query.page != undefined && req.query.perPage != undefined) {
+
+		if (req.query.page <= 0 || req.query.perPage <= 0)
+			res.status(404).send("Pagination parameter out of bounds.");
+
+		//Get customers with pagination
+		db.getCustomers(
+			(req.query.page - 1) * req.query.perPage,
+			req.query.perPage
+		).then((r) => {
+			if (r.length === 0)//If empty return
+				res.status(404).send("Not Found");
+			else
+				res.json(r);
+		}
+		).catch(err => console.log(err));
+
+	} else {//Get all with no filters
+		db.getAllCustomers().then((r) => {
+			if (r.length === 0)
+				res.status(404).send("Not Found");
+			else
+				res.json(r);
+		});
+	}
+});
+
+/**
+ * Responds with specific customer matching the passed id
+ */
+app.get("/api/v1/customers/:id", (req, res) => {
+	db.getCustomer(req.params.id).then((r) => {
+		if (r.length === 0)
+			res.status(404).send("Not Found");
+		else
+			res.json(r);
 	});
 });
 
 
-app.listen(port, ()=>{
-	console.log("Listening on port "+port+"...");
+app.listen(port, () => {
+	console.log("Listening on port " + port + "...");
 });
